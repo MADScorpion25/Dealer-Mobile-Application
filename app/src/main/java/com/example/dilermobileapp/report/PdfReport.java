244b1be07b8c0bd3c2b1af386c61dfe36542fbeb
admal <ad.malkova73@gmail.com>
2022-12-21 01:00:30 +0400

    report logic extend

--- app/src/main/java/com/example/dilermobileapp/report/PdfReport.java
+++ app/src/main/java/com/example/dilermobileapp/report/PdfReport.java
@@ -6,12 +6,16 @@ import com.example.dilermobileapp.config.AlertCreating;
 import com.example.dilermobileapp.declarations.CarLogicDeclaration;
 import com.example.dilermobileapp.declarations.ConfigLogicDeclaration;
 import com.example.dilermobileapp.declarations.ReportLogicDeclaration;
+import com.example.dilermobileapp.declarations.SpecialLogicDeclaration;
 import com.example.dilermobileapp.logic.CarServiceLogic;
 import com.example.dilermobileapp.logic.ConfigServiceLogic;
+import com.example.dilermobileapp.logic.SpecialServiceLogic;
 import com.example.dilermobileapp.models.Car;
 import com.example.dilermobileapp.models.Config;
+import com.example.dilermobileapp.models.Special;
 import com.example.dilermobileapp.storages.CarsCarStorage;
 import com.example.dilermobileapp.storages.ConfigsStorage;
+import com.example.dilermobileapp.storages.SpecialsStorage;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
@@ -29,12 +33,14 @@ public class PdfReport implements ReportLogicDeclaration {
 
     private CarLogicDeclaration carLogic;
     private ConfigLogicDeclaration configLogic;
+    private SpecialLogicDeclaration specialLogic;
     private Context context;
 
     public PdfReport(Context context) {
         carLogic = new CarServiceLogic(new CarsCarStorage());
         this.context = context;
         configLogic = new ConfigServiceLogic(new ConfigsStorage());
+        specialLogic = new SpecialServiceLogic(new SpecialsStorage());
     }
 
     @Override
@@ -53,15 +59,27 @@ public class PdfReport implements ReportLogicDeclaration {
                     paragraph.setTextAlignment(TextAlignment.CENTER);
                     document.add(paragraph);
 
-                    float[] pointColumnWidths = {150F, 150F, 150F};
+                    float[] pointColumnWidths = {150F, 150F, 150F, 150F, 150F};
                     Table table = new Table(pointColumnWidths);
                     table.addCell(new Cell().add(new Paragraph("Config")));
                     table.addCell(new Cell().add(new Paragraph("Power")));
                     table.addCell(new Cell().add(new Paragraph("Price")));
+                    table.addCell(new Cell().add(new Paragraph("Car Class")));
+                    table.addCell(new Cell().add(new Paragraph("Drive Type")));
                     for (Config config : getCarConfigs(car)) {
                         table.addCell(new Cell().add(new Paragraph(config.getConfigurationName())));
                         table.addCell(new Cell().add(new Paragraph(Short.toString(config.getPower()))));
                         table.addCell(new Cell().add(new Paragraph(Integer.toString(config.getPrice()))));
+
+                        if(config.getSpecial() != null) {
+                            Special special = getConfigSpecial(config);
+                            table.addCell(new Cell().add(new Paragraph(special.getCarClass().toString())));
+                            table.addCell(new Cell().add(new Paragraph(special.getDriveType().toString())));
+                        }
+                        else {
+                            table.addCell(new Cell().add(new Paragraph("EMPTY_FIELD")));
+                            table.addCell(new Cell().add(new Paragraph("EMPTY_FIELD")));
+                        }
                     }
                     document.add(table);
                 }
@@ -84,6 +102,10 @@ public class PdfReport implements ReportLogicDeclaration {
 
     private List<Config> getCarConfigs(Car car) {
         return carLogic.getCarConfigs(car);
+    }
+
+    private Special getConfigSpecial(Config config) {
+        return specialLogic.getSpecialById(config.getSpecial().getId());
     }
 
 }
